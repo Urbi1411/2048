3777927bfd3d2a1937518cf56ca17ba11e9954a7
jakub <jakuburbanek@email.cz>
2026-05-07 12:43:53 +0200

    3.0

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from logic import *
 SIZE = 4
 CELL = 100
 
+
 class Game:
     def __init__(self, root):
         self.root = root
-        self.canvas = tk.Canvas(root, width=SIZE*CELL, height=SIZE*CELL)
+
+        self.canvas = tk.Canvas(
+            root,
+            width=SIZE * CELL,
+            height=SIZE * CELL
+        )
         self.canvas.pack()
 
-        self.grid = [[0]*SIZE for _ in range(SIZE)]
+        self.grid = [[0] * SIZE for _ in range(SIZE)]
 
         gen_new_tile(self.grid)
         gen_new_tile(self.grid)
@@ -31,15 +37,40 @@ class Game:
 
                 value = self.grid[i][j]
 
-                self.canvas.create_rectangle(x0, y0, x1, y1, fill="lightgray")
+                self.canvas.create_rectangle(
+                    x0,
+                    y0,
+                    x1,
+                    y1,
+                    fill="lightgray"
+                )
 
                 if value != 0:
                     self.canvas.create_text(
-                        x0 + CELL//2,
-                        y0 + CELL//2,
+                        x0 + CELL // 2,
+                        y0 + CELL // 2,
                         text=str(value),
                         font=("Arial", 20, "bold")
                     )
+
+    def show_message(self, text):
+        self.canvas.create_rectangle(
+            50,
+            150,
+            350,
+            250,
+            fill="white",
+            outline="black",
+            width=3
+        )
+
+        self.canvas.create_text(
+            SIZE * CELL // 2,
+            SIZE * CELL // 2,
+            text=text,
+            font=("Arial", 28, "bold"),
+            fill="red"
+        )
 
     def on_key(self, event):
         key = event.keysym.lower()
@@ -57,11 +88,11 @@ class Game:
         self.draw()
 
         if has_2048(self.grid):
-            print("You won!")
+            self.show_message("YOU WON!")
             self.root.unbind("<Key>")
 
         elif is_grid_full(self.grid) and not can_merge(self.grid):
-            print("You lost!")
+            self.show_message("GAME OVER")
             self.root.unbind("<Key>")
 
 
